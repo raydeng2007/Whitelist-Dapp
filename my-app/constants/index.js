db442707261bc783a16121b5bb2090d4b494c9d9
raydeng2007 <r6deng@uwaterloo.ca>
2022-04-06 13:52:37 -0400

    update constants

--- my-app/constants/index.js
+++ my-app/constants/index.js
@@ -1,67 +1,67 @@
 export const abi = [
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "uint8",
-        name: "_maxWhitelistedAddresses",
-        type: "uint8",
-      },
+        "internalType": "uint8",
+        "name": "_maxWhitelistedAddresses",
+        "type": "uint8"
+      }
     ],
-    stateMutability: "nonpayable",
-    type: "constructor",
+    "stateMutability": "nonpayable",
+    "type": "constructor"
   },
   {
-    inputs: [],
-    name: "addAddressToWhitelist",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
+    "inputs": [],
+    "name": "addAddressToWhitelist",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "maxWhitelistedAddresses",
-    outputs: [
+    "inputs": [],
+    "name": "maxWhitelistedAddresses",
+    "outputs": [
       {
-        internalType: "uint8",
-        name: "",
-        type: "uint8",
-      },
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [],
-    name: "numAddressesWhitelisted",
-    outputs: [
+    "inputs": [],
+    "name": "numAddressesWhitelisted",
+    "outputs": [
       {
-        internalType: "uint8",
-        name: "",
-        type: "uint8",
-      },
+        "internalType": "uint8",
+        "name": "",
+        "type": "uint8"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
+    "stateMutability": "view",
+    "type": "function"
   },
   {
-    inputs: [
+    "inputs": [
       {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
     ],
-    name: "whitelistedAddresses",
-    outputs: [
+    "name": "whitelistedAddresses",
+    "outputs": [
       {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
     ],
-    stateMutability: "view",
-    type: "function",
-  },
+    "stateMutability": "view",
+    "type": "function"
+  }
 ];
 export const WHITELIST_CONTRACT_ADDRESS =
-  "0x98f5c62F7b5Bc735a64942dBec9456954c7a368A";
+  "0x8B2ccA1537cE31Bc81c1f207a30BD83E8d0615bc";
